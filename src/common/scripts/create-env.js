d1612487f4cca187ae19a45138e3e317dccda4c0
Ričards Tagils <tagils@mail.ru>
2019-08-14 16:03:15 +0300

    BE-00 dependeny update

--- src/common/scripts/create-env.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-fs.writeFileSync(
-  './.env',
-  `
-    PORT=${process.env.PORT}\n
-    MONGO_URL=${process.env.MONGO_URL}\n
-    JWT_SECRET=${process.env.JWT_SECRET}\n
-  `,
-);
