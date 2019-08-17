74e4fdf6ddb4e79a17c6aac15bb454938b643b0d
Richard <36774784+Fecony@users.noreply.github.com>
2019-08-17 19:20:36 +0300

    Update create-project.dto.ts

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,6 +1,6 @@
 export class CreateProjectDTO {
   readonly title: string;
-  readonly code: object;
+  readonly code: string;
   readonly description?: string;
   readonly author: string;
   readonly columns: [string];
