6374dc3f6f679266398c75e9e3aeadd51e654895
Richard <36774784+Fecony@users.noreply.github.com>
2019-08-17 19:21:00 +0300

    Update project.interface.ts

--- src/projects/interfaces/project.interface.ts
+++ src/projects/interfaces/project.interface.ts
@@ -2,7 +2,7 @@ import { Document } from 'mongoose';
 
 export interface IProject extends Document {
   title: string;
-  code: object;
+  code: string;
   description?: string;
   author: string;
   columns: [object];
